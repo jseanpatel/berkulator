e2276689bd95ca1360646def0012a959265778fb
Jacob Patel <jseanpatel@gmail.com>
2019-10-26 14:32:08 -0700

    need to be able to click grade distrib

--- scrape.py
+++ scrape.py
@@ -1,24 +1,26 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import json
 
 driver = webdriver.Firefox()
 
-driver.maximize_window()
+driver.get("https://www.berkeleytime.com/catalog")
 
 # Then navigate to a search for each class.
-classes = driver.find_elements_by_class_name("filter-selection-content")
-
-print(len(classes))
-
+classes = driver.find_elements_by_class_name("filter-selection")
 
 def writeToJSONFile(path, fileName, data):
     filePathNameWExt = './' + path + '/' + fileName + '.json'
     with open(filePathNameWExt, 'w') as fp:
         json.dump(data, fp)
 
+for indivClass in classes:
+    click1 = driver.find_element_by_class_name("filter-selection").click()
+    click2 = driver.find_element_by_xpath("//*[text()[contains(., 'See grade distributions')]]")
+    data = {}
+    data['class'] = ''
+    pass
 
-data = {}
-data['key'] = 'value'
+    # writeToJSONFile('./','file-name',data)
 
-# writeToJSONFile('./','file-name',data)
+
+
