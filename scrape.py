0537bdf4a0c2d4756d48406c960869ef5e0808cc
Jacob Patel <jseanpatel@gmail.com>
2019-10-26 13:40:21 -0700

    added assests folder

--- scrape.py
+++ scrape.py
@@ -1,12 +1,24 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import json
 
 driver = webdriver.Firefox()
 
-# Navigate to berkeleytime.
-driver.get("https://berkeleytime.com/catalog")
+driver.maximize_window()
 
 # Then navigate to a search for each class.
 classes = driver.find_elements_by_class_name("filter-selection-content")
 
-print(classes[0])
+print(len(classes))
+
+
+def writeToJSONFile(path, fileName, data):
+    filePathNameWExt = './' + path + '/' + fileName + '.json'
+    with open(filePathNameWExt, 'w') as fp:
+        json.dump(data, fp)
+
+
+data = {}
+data['key'] = 'value'
+
+# writeToJSONFile('./','file-name',data)
